8da34e812bc331893b4441c329f8bf9ab231c0c0
yonghun16 <yonghun16@naver.com>
2024-04-17 00:43:57 +0900

    update

--- source/course1/2-1-3.py
+++ source/course1/2-1-3.py
@@ -1,6 +1,7 @@
-# ex 1-3 : 변수의 선언, 할당, 참조
+# ex 2-1-3 : 변수의 선언, 할당, 참조
 
 lion = "어흥!"
 tiger = lion
 print(lion)
 print(tiger)
+
---
+++ source/course1/2-4-1.py
@@ -0,0 +1,14 @@
+# 2-4-1 : 자료형 확인하기
+
+abc = "abc"       # 변수abc에 문자열 자료 "abc"이 할당됨
+a = "abc"         # 변수a에 문자열 자료 "abc"이 할당됨
+b = 123           # 변수b에 숫자 123이 할당됨
+c = "123"         # 변수c에 문자열 자료 "123"이 할당됨
+d = abc           # 변수d에 변수abc가 할당됨
+
+print(type(a))    # <class 'str'>  'str'은 문자열
+print(type(b))    # <class 'int'>  'int'은 숫자
+print(type(c))    # <class 'str'>  'str'은 문자열
+print(type(d))    # <class 'str'>  'str'은 문자열
+
+
